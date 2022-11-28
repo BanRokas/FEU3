051e8c2fdd9634f9d3106f6403b6def9f9c6d87c
BanRokas <rokas.banaitis@gmail.com>
2022-11-28 10:29:31 +0200

    16 paskaita - JS Įvadas

---
+++ JavaScript/16 paskaita - 11.28 - JS Įvadas/main.js
@@ -0,0 +1,66 @@
+/* console.log("Labas rytas JS'e"); */
+
+//      Kintamieji
+/*
+  string        - žodinis kintamasis rašomas kabutėse: '' / "" / ``
+  number        - skaitinis kintamasis
+  boolean/bool  - loginis kintamsis (true/false)
+
+  null          - tuščias
+  undefined     - neapibrėžtas
+  NaN           - Not a Number
+
+  array         - masyvas
+  object        - objektas
+
+  ----------------------------------------------------------------------------
+
+  var/let/const - kintamųjų ribų nurodymai
+  nameOrSmth    - kintamojo vardas
+  value         - kintamojo reikšmė
+*/
+
+let manoVardas0 = "Rokas";
+let manoVardas1 = 'Rokas';
+let manoVardas2 = `Rokas`;
+
+let manoAmzius0 = 26;
+let manoAmzius1 = 26.2;
+let manoAmzius2 = 26+0.2;
+
+let vedesIrTuriVaiku = false;
+let neVedesIrNeTuriVaiku = true;
+
+// ---------------------------TASK1------------------------------------
+
+let number1 = 5;
+let number2 = 50;
+let number3 = 5.5;
+let number4 = 15;
+let number5 = -5;
+
+let string1 = "Žodis";
+let string2 = "Žodžiai";
+let string3 = "Žodinėlis";
+let string4 = "Žodis ir vėl žodis";
+let string5 = "";
+
+let bool1 = true;
+let bool2 = false;
+
+// ----------------------------------------------------------------------------
+
+//          Operatoriai
+/* 
+            Basic ones
+  +   - sudėtis
+  -   - atimtis
+  *   - daugyba
+  /   - dalyba
+  **  - kėlimas laipsniu (šaknies traukimas)
+  %   - liekanos radimas
+  ++  - didėjimas
+  --  - mažėjimas
+*/
+
+console.log();

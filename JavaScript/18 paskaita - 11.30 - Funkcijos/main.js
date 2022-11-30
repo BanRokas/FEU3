3ee242ed59c6939c9d9ac03d020dfdda9591067d
BanRokas <rokas.banaitis@gmail.com>
2022-11-30 13:23:09 +0200

    18 paskaita - masyvų metodai

--- JavaScript/18 paskaita - 11.30 - Funkcijos/main.js
+++ JavaScript/18 paskaita - 11.30 - Funkcijos/main.js
@@ -91,3 +91,28 @@ function kiekStringuMasyve(array){
   }
   return kiek;
 }
+
+//      Masyvų metodai
+/*
+  pop()       - išima iš masyvo galo, modifikuoja
+  shift()     - išima iš masyvo pradžios, modifikuoja
+  push()      - prideda prie masyvo galo, modifikuoja
+  unshift()   - prideda prie masyvo pradžios, modifikuoja
+
+  reverse()   - apsuka masyvą, modifikuoja masyvą
+  slice(nr1, nr2) - paima nuo nr1 (imtinai) iki nr2 (neimtinai) masyvo elementus, NEmodifikuoja masyvo
+  splice(nr1, nr2, nr3, nr4, nr5...) - nuo nr1 trina nr2 kiekį elementų ir nr1 vietoje įterpia nr3, nr4, nr5... elementus, modifikuoja
+  arr1.concat(arr2, arr3, arr4...) - prie array1 masyvo prijungia arr2, arr3, arr4... masyvus, NEmodifikuoja 
+  sort()      - išrikiuoja masyvą abecelės tvarka.
+    skaičių problema spendžiama su function(a,b){return a-b} įkelta į sort skliaustelius
+    žodžių problema sprendžiama su function(a,b){return a.localeCompare(b)} įkelta į sort skliaustelius
+*/
+
+let masyvas = [1,2,3,4,5,6,7,8,9];
+let gabaliukas = masyvas.slice(3, 6);
+let iskirptas = masyvas[3];
+masyvas.splice(3, 1, 9, 8, 7);
+let sujungtas = masyvas.concat(['lesalas', 'labas', 'ąs', 'naujas', 'ūsas', "žiūrovas"], [false, 0, "undefined", "null", 20, 90, 1110, 654]);
+// sujungtas.sort(function(a,b){return a-b});
+let zodziuMasyvas = ['lesalas', 'labas', 'ąs', 'naujas', 'ūsas', "žiūrovas"];
+zodziuMasyvas.sort();

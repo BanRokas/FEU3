90bd28532a6d219d0018b436d7d89f3570e33b65
BanRokas <rokas.banaitis@gmail.com>
2022-12-07 14:22:30 +0200

    23 paskaita - masyvų užduotys

--- JavaScript/23 paskaita - 12.07 - Lambda ir Masyvų metodai/scripts/main.js
+++ JavaScript/23 paskaita - 12.07 - Lambda ir Masyvų metodai/scripts/main.js
@@ -23,3 +23,66 @@ function daugyba0(par1, par2){
 }
 // naujuoju (lambda/arrow)
 let daugyba1 = (par1, par2) => par1*par2;
+
+function daugyba0(){
+  return "labas";
+}
+
+let uzd1 = () => "labas";
+console.log(uzd1());
+// console.log((() => "Labas!")());
+
+let uzd2 = (par1, par2) => par1+par2;
+
+let uzd3 = (tekstas) => tekstas.length;
+
+let uzd4 = (tekstas) => tekstas.charAt(0);
+
+
+let uzd8 = (masyvas) => {
+  for(let i = 0; i < masyvas.length; i+=2){
+    console.log(masyvas[i]);
+  }
+}
+
+let uzduotis9 = (masyvas1, n) => {
+  for(let element of masyvas1){
+    console.log(element);
+  }
+  // masyvas1.forEach((element, i) =>  i % n == 0 && console.log(element));
+};
+
+
+
+//            Masyvų metodai
+/*Iteraciniai masyvų metodai
+  forEach()
+  map()
+  filter()
+  reduce()
+
+  reduceRight()
+  some()
+  every()
+  find()
+  findIndex()
+  findLast()
+  findLastIndex()
+  flatMap()
+
+  sort()
+
+
+*/
+// populiarus rikiavimo metodas - bubble sort
+let skMas = [5,6,7,1,8,16,51,61,91,9,61,61,321,32,169];
+let zodzMas = ['labas','ate','ąžuolas','ąsotis', 'žodis', 'kanarėlė'];
+
+function skaiciuSortas(a, b){
+  return a - b;
+}
+skMas.sort(skaiciuSortas);
+
+skMas.sort((a, b) => a - b);
+
+console.log(zodzMas.sort((a,b) => a.localeCompare(b)));

def124397dcb53d51cc57b51ada19d4dfe6cd1d2
BanRokas <rokas.banaitis@gmail.com>
2022-12-12 12:07:00 +0200

    26 paskaita - tas3 progress

--- JavaScript/26 paskaita - 12.12 - Interaktyvus DOM/scripts/main.js
+++ JavaScript/26 paskaita - 12.12 - Interaktyvus DOM/scripts/main.js
@@ -35,3 +35,52 @@ submitMygtukoSukurimas();
 
 formSec.append(form);
 
+const saskaita = document.querySelector('#saskaita');
+let saskaitosSukurimas = (preke) => {
+  let mainDiv = document.createElement('div');
+
+  let p1 = document.createElement('p');
+  p1.classList.add('date');
+  let text = document.createTextNode(gautiDabartiniLaika());
+  p1.append(text);
+
+  let h4 = document.createElement('h4');
+  text = document.createTextNode("Įmonės pav");
+  h4.append(text);
+
+  let h1 = document.createElement('h1');
+  text = document.createTextNode('Sąskaita');
+  h1.append(text);
+
+  const hr = document.createElement('hr');
+  const hr1 = document.createElement('hr');
+  const hr2 = document.createElement('hr');
+
+  const ul = document.createElement('ul');
+  // ul
+
+  let p2 = document.createElement('p');
+  p2.classList.add('bendraKaina');
+  text = document.createTextNode('bendraKaina');
+  p2.append(text);
+
+  let h2 = document.createElement('h2');
+  text = document.createTextNode('Padėkojimas');
+  h2.append(text);
+
+  mainDiv.append(p1, h4, h1, hr, ul, hr1, p2, hr2, h2);
+  saskaita.append(mainDiv);
+}
+
+let gautiDabartiniLaika = () => {
+  // let date = new Date().toISOString();
+  // date = date.replace("T", " ");
+  // date = date.slice(0, date.indexOf(" ")+1)+
+  //         (Number(date.slice(date.indexOf(" ")+1, date.indexOf(" ")+3))+2)+
+  //         date.slice(date.indexOf(" ")+3, date.indexOf(" ")+6);
+  let date = new Date();
+  let dateString = date.toLocaleString('lt-LT');
+  return dateString;
+}
+
+saskaitosSukurimas();

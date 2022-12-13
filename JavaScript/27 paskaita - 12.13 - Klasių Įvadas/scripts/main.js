7941edc0ab11d0559ed6cc58d97f298e85858e61
BanRokas <rokas.banaitis@gmail.com>
2022-12-13 14:25:44 +0200

    27 paskaita - 1task ats

--- JavaScript/27 paskaita - 12.13 - Klasių Įvadas/scripts/main.js
+++ JavaScript/27 paskaita - 12.13 - Klasių Įvadas/scripts/main.js
@@ -52,45 +52,73 @@ let asmenysBeKlasiu = [
   console.log(asmenysSuKlasemis[0].pasisveikinaSu("Jonas"));
 }
 
-class Ledai{
-  constructor(pavadinimas, skonis, kaina){
-    this.pavadinimas = pavadinimas;
-    this.skonis = skonis;
-    this.kaina = kaina;
+// ledų užduotis kartu
+{
+  class Ledai{
+    constructor(pavadinimas, skonis, kaina){
+      this.pavadinimas = pavadinimas;
+      this.skonis = skonis;
+      this.kaina = kaina;
+    }
+    kainosKeitimas(keitimasProcentais){ // neigiamas % - kaina mažinama
+      return this.kaina + this.kaina * keitimasProcentais/100; 
+      //        0.60    +     0.60   *        -37        /100
+    }
   }
-  kainosKeitimas(keitimasProcentais){ // neigiamas % - kaina mažinama
-    return this.kaina + this.kaina * keitimasProcentais/100; 
-    //        0.60    +     0.60   *        -37        /100
-  }
+
+  const leduMasyvas = [
+    new Ledai("Dadu", "karamelinis", 1.201354),
+    new Ledai("Tirpuko", "avietinis", 0.4941132),
+    new Ledai("Nykštukas", "vanilinis", 1.505816)
+  ];
+
+  console.log(leduMasyvas);
+
+  console.groupCollapsed('Ledų pavadinimai');
+  leduMasyvas.forEach(ledas => console.log(ledas.pavadinimas));
+  console.groupEnd();
+
+  console.groupCollapsed('Ledų skoniai');
+  leduMasyvas.forEach(ledas => console.log(ledas.skonis));
+  console.groupEnd();
+
+  console.groupCollapsed('Ledų kainos');
+  leduMasyvas.forEach(ledas => console.log(ledas.kaina.toFixed(2)));
+  console.groupEnd();
+
+  console.groupCollapsed('Ledų kainos su 37% nuolaida');
+  leduMasyvas.forEach(ledas => console.log(ledas.kainosKeitimas(-37).toFixed(2)));
+  console.groupEnd();
+
+  console.groupCollapsed('Padidinom ledu kainų duomenis');
+  leduMasyvas.forEach(ledas => {
+    ledas.kaina += 0.1;
+    console.log(ledas.kaina);
+  });
+  console.groupEnd();
 }
 
-const leduMasyvas = [
-  new Ledai("Dadu", "karamelinis", 1.201354),
-  new Ledai("Tirpuko", "avietinis", 0.4941132),
-  new Ledai("Nykštukas", "vanilinis", 1.505816)
-];
+// užduotys
 
-console.log(leduMasyvas);
+// 1
+{
+  class Car{
+    constructor(name, year){
+      this.name = name;
+      this.year = year;
+      this.helloCar = `Labas, čia mano mašina ${this.name} ir ji buvo pagaminta ${this.year} metais.`;
+    }
+    age(){
+      return new Date().getFullYear() - this.year;
+    }
+  }
 
-console.groupCollapsed('Ledų pavadinimai');
-leduMasyvas.forEach(ledas => console.log(ledas.pavadinimas));
-console.groupEnd();
+  const cars = [
+    new Car('Volvo', 1990),
+    new Car("Fiat", 2001),
+    new Car("Audi", 2005)
+  ];
 
-console.groupCollapsed('Ledų skoniai');
-leduMasyvas.forEach(ledas => console.log(ledas.skonis));
-console.groupEnd();
-
-console.groupCollapsed('Ledų kainos');
-leduMasyvas.forEach(ledas => console.log(ledas.kaina.toFixed(2)));
-console.groupEnd();
-
-console.groupCollapsed('Ledų kainos su 37% nuolaida');
-leduMasyvas.forEach(ledas => console.log(ledas.kainosKeitimas(-37).toFixed(2)));
-console.groupEnd();
-
-console.groupCollapsed('Padidinom ledu kainų duomenis');
-leduMasyvas.forEach(ledas => {
-  ledas.kaina += 0.1;
-  console.log(ledas.kaina);
-});
-console.groupEnd();
+  console.log(cars[0].helloCar);
+  console.log(cars[0].age());
+}

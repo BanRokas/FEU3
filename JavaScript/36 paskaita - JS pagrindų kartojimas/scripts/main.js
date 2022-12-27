d236b11942869efb72cbd06e7ffdfc44a11ca9ff
BanRokas <rokas.banaitis@gmail.com>
2022-12-27 09:11:05 +0200

    36 paskaita - JS Kartojimas

---
+++ JavaScript/36 paskaita - JS pagrindų kartojimas/scripts/main.js
@@ -0,0 +1,39 @@
+/*              JS Pagrindai
+  1) Kintamieji
+    String
+    Number
+    Boolean
+    Array
+    Object
+  2) Operatoriai
+    Matematiniai (+ trumpiniai)
+    Palyginimo
+    Loginiai
+  3) Sąlygos
+    if (if else; if elseIf else...)
+    switch
+    ternary (salyga?tiesa:melas) (salyga&&tiesa)
+  4) Ciklai
+    for   (for in; for of)
+    while (do while)
+    iteraciniai masyvų metodai (forEach, map, filter, reduce)
+  5) Funkcijos
+    paprastas funkcijos deklaravimas (senasis būdas)
+    anoniminės
+    arrow/lambda
+  6) Metodai
+    Number/Math
+    String
+    Array
+    Object
+  7) Destruktūrizavimas
+*/
+/*
+  0) CRUD pasibaigti.
+  1) Konspektuotis (jeigu konspektą turit - persiskaityti/pasikartoti)
+  2) Daryti užduotis:
+    js array,string,numbers.txt
+    JS ciklai ir funkcijos2.txt
+    JS array,string methods practice.txt
+    JS kartojimas ir HigherOrderFunctions.txt
+*/

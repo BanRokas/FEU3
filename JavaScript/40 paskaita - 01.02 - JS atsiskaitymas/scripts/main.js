8923687d26426029f32752529a7cc60bbf3ab71d
BanRokas <rokas.banaitis@gmail.com>
2023-01-02 16:02:46 +0200

    40 paskaita - JS atsiskaitymas

---
+++ JavaScript/40 paskaita - 01.02 - JS atsiskaitymas/scripts/main.js
@@ -0,0 +1,3 @@
+import { function1, function2, function3 as f3 } from './middleware/functions.js';
+
+console.log(f3());
---
+++ JavaScript/40 paskaita - 01.02 - JS atsiskaitymas/scripts/middleware/functions.js
@@ -0,0 +1,13 @@
+export let function1 = () => {
+  return 'function1 return thingie';
+}
+
+export let function2 = () => {
+  return 'function2 return thingie';
+}
+
+export let function3 = () => {
+  return 'function3 return thingie';
+}
+
+// export { function1, function2, function3 };

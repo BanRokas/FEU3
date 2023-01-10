56e6b54bc5d36d18ae973f05e1eeaebe42374bf3
BanRokas <rokas.banaitis@gmail.com>
2023-01-10 14:46:36 +0200

    46 paskaita - užduotis

--- React/46 paskaita - 01.10 - React įvadas/react_bandymas/src/App.jsx
+++ React/46 paskaita - 01.10 - React įvadas/react_bandymas/src/App.jsx
@@ -1,7 +1,21 @@
+import "./app.css";
+
 const App = () => {
   return (
-    <>
-    </>
+  <>
+    <h1>Labas Lietuva!</h1>
+    <p>Neveikia for some reason?</p>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolore voluptas voluptatem enim commodi reprehenderit sed, quos ducimus, dolor adipisci debitis eligendi similique rem quam magnam tenetur possimus. Reprehenderit, voluptatem.
+    </h1>
+    <ul>
+      <li><a href="https:" className="link">a</a></li>
+      <li><a href="https:" className="link">b</a></li>
+      <li><a href="https:" className="link">c</a></li>
+      <li><a href="https:" className="link">d</a></li>
+      <li><a href="https:" className="link">e</a></li>
+    </ul>
+  </>
   );
 }
 

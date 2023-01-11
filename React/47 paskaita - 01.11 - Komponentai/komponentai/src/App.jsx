bc7775d5145233e4311c330c0b9d5c3ed77ffd83
BanRokas <rokas.banaitis@gmail.com>
2023-01-11 10:21:25 +0200

    47 paskaita - Komponentai

---
+++ React/47 paskaita - 01.11 - Komponentai/komponentai/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './components/Header';
+import List from './components/List';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="klientuSarasas">
+        <h2>Klientų sąrašas</h2>
+        <List 
+          data = {
+            ['Petras', 'Rokas', 'Jonas', 'Kamilė']
+          }
+        />
+      </div>
+      <div className="prekiuSarasas">
+        <h2>Prekių sąrašas</h2>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
